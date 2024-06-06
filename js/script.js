c385ea8f19a1defc73e6f3eecb3ae1b62ebfdc2f
saka-C <shaka.aufa.alhajar@gmail.com>
2024-06-06 14:55:25 +0700

    add index page

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ function selectRadioButton(id, describeElement) {
 function goBack() {
     window.history.back(); // Fungsi untuk kembali ke halaman sebelumnya
 }
+
+
+// scrolled
+// ================
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+})
